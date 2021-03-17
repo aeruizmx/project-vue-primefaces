a0adf39d1a4b65826be614c9c05c6409d0ecc4af
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-17 11:29:10 -0600

    Composition api config

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <InputText v-model="text" />
+  <Button label="Greetings" icon="pi pi-user" @click="greet"></Button>
+  <Toast />
+  {{text}}
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {defineComponent, ref} from 'vue';
+import {useToast} from 'primevue/usetoast';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default defineComponent({
+  setup(){
+    const toast = useToast();
+    const text = ref('Prime');
+    const greet = () => {
+      toast.add({severity:'info', summary: 'Hello', detail: text, life: 3000});
+    }
+    return {
+      text,
+      greet
+    }
   }
-}
+})
+/* export default {
+  data(){
+    return {
+      text: null
+    }
+  },
+  methods: {
+    greet(){
+      this.$toast.add({severity:'info', summary: 'Hello', detail:this.text, life: 3000});
+    }
+  }
+} */
 </script>
 
 <style>
